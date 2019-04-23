7626c4f97b8214b222ff4c27fab980bf0351bee0
Matt Moore <mattmoore@carbonhelix.com>
2019-04-23 18:34:47 +0000

    Use docker-entrypoint.sh and call entrypoint from docker-compose.yml.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+/kafka/bin/kafka-server-start.sh /kafka/config/server.properties \
+  --override zookeeper.connect=zookeeper:2181 \
+  --override advertised.listeners=PLAINTEXT://127.0.0.1:9092
