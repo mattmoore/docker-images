023f084a396013bc544141380c406d4f74742ce2
Matt Moore <mattmoore@carbonhelix.com>
2019-04-26 12:25:31 -0500

    WIP: Get Kafka working with docker externally and internally.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 /kafka/bin/kafka-server-start.sh /kafka/config/server.properties \
   --override zookeeper.connect=zookeeper:2181 \
-  --override advertised.listeners=PLAINTEXT://127.0.0.1:9092
+  --override listener.security.protocol.map=EXTERNAL:PLAINTEXT,INTERNAL:PLAINTEXT \
+  --override inter.broker.listener.name=INTERNAL \
+  --override listeners=EXTERNAL://0.0.0.0:29092,INTERNAL://0.0.0.0:9092 \
+  --override advertised.listeners=EXTERNAL://localhost:29092,INTERNAL://kafka:9092
