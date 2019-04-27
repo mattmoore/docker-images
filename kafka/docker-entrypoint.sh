9af4d83333cfd7f25d8432ca9d51e63d986e3bfe
Matt Moore <mattmoore@carbonhelix.com>
2019-04-27 16:54:46 +0000

    Use standard kafka broker port for external traffic.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -4,5 +4,5 @@
   --override zookeeper.connect=zookeeper:2181 \
   --override listener.security.protocol.map=EXTERNAL:PLAINTEXT,INTERNAL:PLAINTEXT \
   --override inter.broker.listener.name=INTERNAL \
-  --override listeners=EXTERNAL://0.0.0.0:29092,INTERNAL://0.0.0.0:9092 \
-  --override advertised.listeners=EXTERNAL://localhost:29092,INTERNAL://kafka:9092
+  --override listeners=EXTERNAL://0.0.0.0:9092,INTERNAL://0.0.0.0:29092 \
+  --override advertised.listeners=EXTERNAL://kafka:9092,INTERNAL://kafka:29092
